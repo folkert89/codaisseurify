925a43e0c2074cdbbd4dd2b59b2fdff9239d4c96
folkert89 <folkertburghout@hotmail.com>
2018-01-20 19:30:50 +0100

    TDD starts with 1 feature and two models

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+      it "is invalid without name" do
+        artist = Artist.new(name: "")
+        artist.valid?
+        expect(artist.errors).to have_key(:name)
+      end
+      it "is invalid without release date" do
+        artist = Artist.new(birth_date: "")
+        artist.valid?
+        expect(artist.errors).to have_key(:birth_date)
+      end
+      it "is invalid without genre" do
+        artist = Artist.new(country_of_origin: "")
+        artist.valid?
+        expect(artist.errors).to have_key(:country_of_origin)
+      end
+    end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+      it "is invalid without name" do
+        song = Song.new(name: "")
+        song.valid?
+        expect(song.errors).to have_key(:name)
+      end
+      it "is invalid without release date" do
+        song = Song.new(release_date: "")
+        song.valid?
+        expect(song.errors).to have_key(:release_date)
+      end
+      it "is invalid without genre" do
+        song = Song.new(genre: "")
+        song.valid?
+        expect(song.errors).to have_key(:genre)
+      end
+      it "is invalid without an artist" do
+        song = Song.new
+        song.valid?
+        expect(song.errors).to have_key(:artist)
+      end
+    end
 end
