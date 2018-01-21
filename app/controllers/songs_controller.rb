0e6ce11a58da97a19a1f46eca841a958aaa08a46
folkert89 <folkertburghout@hotmail.com>
2018-01-21 08:29:12 +0100

    Add new song functionality and nested routing songs and generated controller songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,26 @@
 class SongsController < ApplicationController
-  
+    before_action :load_artist
+    def new
+      @song = @artist.songs.new
+    end
+
+    def create
+      @song = @artist.songs.new(song_params)
+
+      if @song.save
+        redirect_to @artist, notice: "song created"
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def load_artist
+      @artist = Artist.find(params[:artist_id])
+    end
+
+    def song_params
+      params.require(:song).permit(:name, :release_date, :genre)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "artists#index"
 
-  resources :songs, except: [:edit, :update]
-  resources :artists, except: [:new, :edit, :create, :update]
+
+  resources :artists, except: [:new, :edit, :create, :update] do
+      resources :songs, except: [:edit, :update]
+  end
 end
