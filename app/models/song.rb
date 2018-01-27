93ce5e43d936fd7c7c33b1283ff5bf21c62c2a22
folkert89 <folkertburghout@hotmail.com>
2018-01-27 16:51:05 +0100

    add API artists

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,4 @@ class Song < ApplicationRecord
   belongs_to :artist
 
   validates :name, presence: true
-  validates :genre, presence: true
-  validates :release_date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root "artists#index"
 
-
   resources :artists, except: [:new, :edit, :create, :update] do
       resources :songs, except: [:edit, :update]
+  end
+
+  namespace :api do
+    resources :artists, except: [:new, :edit, :create, :update] do
+        resources :songs, except: [:edit, :update]
+    end
   end
 end
