7f87ef3659352d88eeb79337e98a3a030ce8060c
folkert89 <folkertburghout@hotmail.com>
2018-01-28 18:51:13 +0100

    feedback arno applied, but unfortunately not worked in branches

---
+++ spec/features/list_artists_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+#
+#
+# describe "Visitors can see a list of all the artists" do
+#
+#   it "shows all artists" do
+#     visit root_url
+#
+#     expect(page).to have_text("All artists")
+#   end
+# end
+# # first test message. no visibible link or button "Paul McCartney"
